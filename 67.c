2e0158636477d97355b7973a4f2d59a65c2f5ac8
nonpolarity <nonpolarity@gmail.com>
2015-09-12 15:19:58 -0700

    KO\!

--- 67.c
+++ 67.c
@@ -11,25 +11,25 @@ addBinary(char *a, char *b)
 	char           *c = (char *)malloc(sizeof(char) * (lenth + 2));
 	c[lenth + 1] = '\0';
 	for (i = lenth; i > 0; i--) {
-		printf("i:%d\n", i);
+		//printf("i:%d\n", i);
 		int		A         , B, C;
 		if ((lenth - i + 1) <= strlen(a))
 			A = a[strlen(a) - 1 - lenth + i] - '0';
 		else
 			A = 0;
-		printf("A:%c\n", A);
+		//printf("A:%c\n", A);
 		if ((lenth - i) + 1 <= strlen(b))
 			B = b[strlen(b) - 1 - lenth + i] - '0';
 		else
 			B = 0;
-		printf("B:%c\n", B);
+		//printf("B:%c\n", B);
 		C = A ^ B ^ cin;
-		printf("c[i]:%d\n", A ^ B ^ cin);
+		//printf("c[i]:%d\n", A ^ B ^ cin);
 		cout = (A & B) | (cin & (A | B));
 
 		cin = cout;
 		c[i] = C + '0';
-		printf("cin:%c\n\n", cin);
+		//printf("cin:%c\n\n", cin);
 	}
 	if (cout == 1) {
 		c[0] = '1';
