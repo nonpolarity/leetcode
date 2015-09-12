573e4c65a98ecbf04143d8fa407ef3b0576774e5
nonpolarity <nonpolarity@gmail.com>
2015-09-12 13:21:45 -0700

    first success

--- 67.c
+++ 67.c
@@ -1,42 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char           *
 addBinary(char *a, char *b)
 {
-	long int	x = 0,	y = 0, z = 0;
 	int		i = 0,	lenth = 0;
-	lenth = sizeof(&a) > sizeof(&b) ? sizeof(&a) : sizeof(&b);
-	char           *c = (char *)malloc(lenth * 4);
-	for (i = 0; a[i] != '\0'; i++) {
-		x = (a[i] - '0') + x * 2;
+	int		cin = 0,	cout = 0;
+	lenth = strlen(a) > strlen(b) ? strlen(a) : strlen(b);
+	char           *c = (char *)malloc(sizeof(char) * (lenth + 2));
+	c[lenth + 1] = '\0';
+	for (i = lenth; i > 0; i--) {
+		printf("i:%d\n", i);
+		int		A         , B, C;
+		if ((lenth - i + 1) <= strlen(a))
+			A = a[strlen(a) - 1 - lenth + i] - '0';
+		else
+			A = 0;
+		printf("A:%c\n", A);
+		if ((lenth - i) + 1 <= strlen(b))
+			B = b[strlen(b) - 1 - lenth + i] - '0';
+		else
+			B = 0;
+		printf("B:%c\n", B);
+		C = A ^ B ^ cin;
+		printf("c[i]:%d\n", A ^ B ^ cin);
+		cout = (A & B) | (cin & (A | B));
+
+		cin = cout;
+		c[i] = C + '0';
+		printf("cin:%c\n\n", cin);
 	}
-	for (i = 0; b[i] != '\0'; i++) {
-		y = (b[i] - '0') + y * 2;
+	if (cout == 1) {
+		c[0] = '1';
+		return c;
 	}
-	z = x + y;
-	for (i = 0; z > 0; i++) {
-		c[i] = (z - z / 2 * 2) + '0';
-		z = z / 2;
-		if (z < 1) {
-			i++;
-			break;
-		}
-	}
-	c[i] = '\0';
-	int		j;
-	char		temp;
-	for (j = 0; j < i / 2; j++) {
-		temp = c[j];
-		c[j] = c[i - 1 - j];
-		c[i - 1 - j] = temp;
-	}
-	return c;
+	//
+		else
+		//c[0] = '0';
+	return c + 1;
 }
 
 int
 main(int argc, char *argv[])
 {
-	printf("the Binary sum of %s and %s is %s\n", argv[1], argv[2], addBinary(argv[1], argv[2]));
+	printf("the Binary sum of %s and %s is %s\n", argv[1], argv[2],
+	       addBinary(argv[1], argv[2]));
 	return 0;
 }
