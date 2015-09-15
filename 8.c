0ee71fcc9dae4f92bb7d4f30f6b25b3c306b081f
nonpolarity <nonpolarity@gmail.com>
2015-09-14 19:36:28 -0700

    KO

--- 8.c
+++ 8.c
@@ -1,36 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 int
 myAtoi(char *str)
 {
-	if (str == 0)
+	//skip the spaces
+	while (*str == ' ')
+		str++;
+	if (('9' < *str | '0' > *str) & (*str != '+') & (*str != '-'))
 		return 0;
-	int		i = 0,	num = 0, sgn = 1;
-	if (str[0] == '-') {
-		i = 1;
-		sgn = -1;
+	int		sgn = 1;
+	unsigned long long num = 0;
+	if ((*str == '-') | (*str == '+')) {
+		if ((*(str + 1) == '-') | (*(str + 1) == '+'))
+			return 0;
+		if (*str == '-') {
+			sgn = -1;
+			str++;
+		} else
+			str++;
 	}
-	if (str[0] == '+') {
-		i = 1;
-	}
-	if ((i == 1) & ((str[1] - '9') > 9 | (str[1] - '0') < 0))
-		return 0;
-	for (; str[i] != '\0'; i++) {
-		if (0 <= str[i] - '0' & str[i] - '0' <= 9)
-			num = num * 10 + str[i] - '0';
-		if (str[i]=='-')
-			sgn*=-1;
+	for (; ('0' <= *str & *str <= '9'); str++) {
+		num = num * 10 + *str - '0';
+		if ((num >= (unsigned long long)INT_MAX) & (sgn == 1))
+			return INT_MAX;
+		if ((num > (unsigned long long)INT_MAX) & (sgn == -1))
+			return INT_MIN;
 	}
 	return num * sgn;
 }
-
-
 int
 main(int argc, char *argv[])
 {
-	char		string    [] = "     010";
-	printf("The atoi of 123 is %d\n", myAtoi(string));
+	char		string    [] = "-2147483648";
+	printf("The atoi of %s is %d\n", string, myAtoi(string));
 	return 0;
 }
