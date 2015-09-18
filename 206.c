caa0e83d9eb3fb3f17ac173e6da814540d617c0e
nonpolarity <nonpolarity@gmail.com>
2015-09-17 20:42:54 -0700

    why can it reach the second part of an OR?

--- 206.c
+++ 206.c
@@ -3,37 +3,42 @@
 #include <string.h>
 #include <stdbool.h>
 
-
-
 struct ListNode {
 	int		val;
 	struct ListNode *next;
 };
 
-
 struct ListNode *
 reverseList(struct ListNode *head)
 {
-	struct ListNode *end = head;
-	if (end == 0)
-		return 0;
-	if (end->next == 0)
+	//if (head == 0)
+		//return 0;
+	if (head == 0 | head->next == 0)
 		return head;
-	struct ListNode *tmp1=head, *tmp2=head->next;
-	while (tmp2 != 0) {
-		tmp1 = tmp2;
-		tmp2 = tmp2->next;
-		tmp1->next = end;
-		end = tmp1;
-		tmp1=tmp2;
-	}
+	struct ListNode *tmp = reverseList(head->next);
+	head->next->next = head;
 	head->next = 0;
-	return end;
-
+	return tmp;
 }
 
 int
 main(int argc, char *argv[])
 {
+	//struct ListNode *node1 = (struct ListNode *)malloc(sizeof(struct ListNode));
+	struct ListNode *node1 = 0;
+	struct ListNode *node2 = (struct ListNode *)malloc(sizeof(struct ListNode *));
+	struct ListNode *node3 = (struct ListNode *)malloc(sizeof(struct ListNode *));
+	struct ListNode *newhead = (struct ListNode *)malloc(sizeof(struct ListNode *));
+	//node1->val = 1;
+	//node1->next = node2;
+	node2->val = 2;
+	node2->next = node3;
+	node3->val = 3;
+	node3->next = 0;
+	newhead = reverseList(node1);
+	while (newhead != 0) {
+		printf("val:%d\n", newhead->val);
+		newhead = newhead->next;
+	}
 	return 0;
 }
