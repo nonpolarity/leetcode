82dcded39da7672c378382aaf7148ecba9dcff33
nonpolarity <nonpolarity@gmail.com>
2015-09-20 15:21:29 -0700

    KO

--- 101.c
+++ 101.c
@@ -9,32 +9,8 @@ struct TreeNode {
 	struct TreeNode *left;
 	struct TreeNode *right;
 };
-
-int
-bfs(struct TreeNode *root, bool leftfirst, struct TreeNode *a[100])
-{
-	memset(a, 0, sizeof(int) * 100);
-	int		head = 0,	tail = 0;
-	a[head++] = root;
-	struct TreeNode *tmp;
-	while (head != tail) {
-		tmp = a[tail++];
-		if (leftfirst == 1) {
-			if (tmp->left != 0)
-				a[head++] = tmp->left;
-			if (tmp->right != 0)
-				a[head++] = tmp->right;
-
-		} else {
-			if (tmp->right != 0)
-				a[head++] = tmp->right;
-			if (tmp->left != 0)
-				a[head++] = tmp->left;
-		};
-	}
-	return tail - 1;
-}
-
+bool
+sym(struct TreeNode *left, struct TreeNode *right);
 bool
 isSymmetric(struct TreeNode *root)
 {
@@ -44,25 +20,37 @@ isSymmetric(struct TreeNode *root)
 		return 1;
 	if (root->left == 0 | root->right == 0)
 		return 0;
-	struct TreeNode *left[100];
-	struct TreeNode *right[100];
-	int		i         , depth_left, depth_right;
-	depth_left = bfs(root->left, 1, left);
-	depth_right = bfs(root->right, 0, right);
-	if (depth_left != depth_right)
-		return 0;
-	for (i = 0; i <= depth_left; i++) {
-		if (left[i]->val != right[i]->val)
-			return 0;
-	};
-	return 1;
+	return sym(root->left, root->right);
 }
+
+bool
+sym(struct TreeNode *left, struct TreeNode *right)
+{
+	if (left == 0 & right == 0)
+		return 1;
+	if (left == 0 & right != 0)
+		return 0;
+	if (left != 0 & right == 0)
+		return 0;
+	if (left->val != right->val)
+		return 0;
+	if (left->left == 0 & right->right != 0)
+		return 0;
+	if (left->right == 0 & right->left != 0)
+		return 0;
+	if (left->left == 0 & left->right == 0 & right->left == 0 & right->right == 0)
+		return 1;
+	return (sym(left->left, right->right) & sym(left->right, right->left));
+}
+
 int
 main(int argc, char *argv[])
 {
-	//struct TreeNode c = {2, 0, 0};
-	struct TreeNode	b = {2, 0, 0};
-	struct TreeNode	a = {1, &b, 0};
+	struct TreeNode	e = {3, 0, 0};
+	struct TreeNode	d = {3, 0, 0};
+	struct TreeNode	c = {2, &d, 0};
+	struct TreeNode	b = {2, 0, &d};
+	struct TreeNode	a = {1, &b, &c};
 	struct TreeNode *p = &a;
 	printf("a:%d\n", isSymmetric(p));
 	return 0;
