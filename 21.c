6e275d0877a15b4952c1d4d68aa11f65050eae44
nonpolarity <nonpolarity@gmail.com>
2015-09-20 15:31:55 -0700

    Revert "KO"
    This reverts commit 0a7eebfa9d2552c2c1d79731f1dc41a98f1881f5.
    
    	modified:   21.c

--- 21.c
+++ 21.c
@@ -3,18 +3,40 @@
 #include <string.h>
 #include <stdbool.h>
 
-int
-removeElement(int *nums, int numsSize, int val)
+struct ListNode {
+	int		val;
+	struct ListNode *next;
+};
+
+
+struct ListNode *
+mergeTwoLists(struct ListNode *l1, struct ListNode *l2)
 {
-	int		i = numsSize - 1;
-	for (; i >= 0; i--) {
-		if (nums[i] == val) {
-			if (i != (numsSize - 1))
-				nums[i] = nums[numsSize - 1];
-			numsSize--;
+	struct ListNode *l = (struct ListNode *)malloc(sizeof(struct ListNode)),
+	               *head = 0;
+	if (l1 == 0 & l2 == 0)
+		return l1;
+	while (l1 != 0 & l2 != 0) {
+		if (l1->val < l2->val) {
+			l->next = l1;
+			l1 = l1->next;
+		} else {
+			l->next = l2;
+			l2 = l2->next;
 		}
+
+
+		if (head == 0)
+			head = l->next;
+		l = l->next;
 	}
-	return numsSize;
+	if (l1 == 0)
+		l->next = l2;
+	else
+		l->next = l1;
+	if (head == 0)
+		head = l->next;
+	return head;
 }
 
 int
