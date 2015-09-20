b3975b833ab522f8f5362310be5e7591a0b5e776
nonpolarity <nonpolarity@gmail.com>
2015-09-20 15:36:12 -0700

    KO

---
+++ 27.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+int
+removeElement(int *nums, int numsSize, int val)
+{
+	int		i = numsSize - 1;
+	for (; i >= 0; i--) {
+		if (nums[i] == val) {
+			if (i != (numsSize - 1))
+				nums[i] = nums[numsSize - 1];
+			numsSize--;
+		}
+	}
+	return numsSize;
+}
+
+int
+main(int argc, char *argv[])
+{
+	return 0;
+}
