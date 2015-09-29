f04d34d768e51fd4395d9a5663622bf6531fca13
nonpolarity <nonpolarity@gmail.com>
2015-09-28 18:08:35 -0700

    the reuslt is OK but leetcode result shows false

---
+++ 36.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+bool
+vaildlinev(char *root, int Col)
+{
+	int		a          [10];
+	memset(a, 0, sizeof(a));
+	for (int i = 0; i < 9; i++) {
+		if (*(root + Col * i) != '.') {
+			int		value = *(root + Col * i) - '0';
+			if (a[value] == 1)
+				return 0;
+			else
+				a[value] = 1;
+		}
+	}
+	return 1;
+}
+bool
+vaildlineh(char *root)
+{
+	int		a          [10];
+	memset(a, 0, sizeof(a));
+	for (int i = 0; i < 9; i++) {
+		if (*(root + i) != '.') {
+			int		value = *(root + i) - '0';
+			if (a[value] == 1)
+				return 0;
+			else
+				a[value] = 1;
+		}
+	}
+	return 1;
+};
+bool
+vaildsquare(char *root, int Col)
+{
+	int		a          [10];
+	memset(a, 0, sizeof(a));
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (*(root + Col * i + j) != '.') {
+				int		value = *(root + Col * i + j) - '0';
+				if (a[value] == 1)
+					return 0;
+				else
+					a[value] = 1;
+			}
+		}
+	}
+	return 1;
+}
+
+bool
+isValidSudoku(char **board, int boardRowSize, int boardColSize)
+{
+	int		i         , j;
+
+	for (i = 0; i < boardRowSize - 8; i++)
+		for (j = 0; j <
+		     boardColSize; j++) {
+			if (!vaildlinev(*board + i * boardColSize +
+					j, boardColSize))
+				return 0;
+		}
+
+	for (i = 0; i < boardRowSize; i++)
+		for (j = 0; j < boardColSize - 8;
+		     j++) {
+			if (!vaildlineh(*board + i * boardColSize + j))
+				return 0;
+		}
+
+
+	for (i = 0; i < boardRowSize - 2; i++)
+		for (j = 0; j < boardColSize - 2; j++) {
+			if (!vaildsquare(*board + i * boardColSize
+					 + j, boardColSize))
+				return 0;
+		}
+
+	return 1;
+}
+
+int
+main(int argc, char *argv[])
+{
+	char		tmp       [9][9] = {".87654321", "2........", "3........", "4........", "5........", "6........", "7........", "8........", "9........"};
+	char           *board[9];
+	for (int i = 0; i < 9; i++)
+		board[i] = tmp[i];
+	//char         *board[] = {tmp[0]};
+
+	//char         *tmp1 = tmp[0];
+	int		boardRowSize = 9;
+	int		boardColSize = 9;
+	printf("the array:\t%d\n", isValidSudoku(board, boardRowSize, boardColSize));
+	return 0;
+}
