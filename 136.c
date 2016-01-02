8da4ee2dedb3276cfd4e7fe2cc3f42eca796a0a4
Alvin Chen <nonpolarity@gmail.com>
2016-01-01 22:29:00 -0800

    bit opertation

--- 136.c
+++ 136.c
@@ -5,17 +5,28 @@
 
 int singleNumber(int *nums, int numsSize)
 {
+	int len = sizeof(int) * 8;
+	int *list = malloc(sizeof(int) * len);
+	memset(list, 0, sizeof(int) * len);
 	int result = 0;
-	for (int i = 0; i < numsSize; i++)
-		result ^= nums[i];
+	int prefix = 1;
+	for (int i = 0; i < len; i++) {
+		for (int j = 0; j < numsSize; j++) {
+			list[i] += (nums[j] - ((nums[j] >> 1) * 2));
+			nums[j] = nums[j] >> 1;
+		}
+		// If there are always triple numbers, then %3;
+		list[i] %= 2;
+	}
+	for (int i = 0; i < len; i++)
+		result += (list[i] << i);
 	return result;
-
 }
 
 int main(int argc, char *argv[])
 {
-	int nums[] = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7 };
-	int numsSize = 13;
+	int nums[] = { 1, 3, 1, -100000, 3 };
+	int numsSize = 5;
 	int result = singleNumber(nums, numsSize);
 	printf("Result: %d\n", result);
 	return 0;
