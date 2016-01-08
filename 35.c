c4efddd92c866b3351071d2044bc424b2181969e
Alvin Chen <nonpolarity@gmail.com>
2016-01-08 15:52:06 -0800

    binary search

--- 35.c
+++ 35.c
@@ -2,22 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+int binarysearch(int *nums, int low, int high, int target)
+{
+	while (1) {
+		int mid = (low + high) / 2;
+		if (nums[mid] == target)
+			return mid;
+		if (nums[mid] > target)
+			high = mid;
+		else
+			low = mid;
+		if (high - low == 1)
+			return high;
+	}
+}
 
 int searchInsert(int *nums, int numsSize, int target)
 {
-	for (int i = 0; i < numsSize; i++) {
-		if (nums[i] >= target)
-			return i;
-	}
-	return numsSize;
-
+	if (nums[numsSize - 1] <= target)
+		return (nums[numsSize - 1] == target) ? numsSize - 1 : numsSize;
+	if (nums[0] >= target)
+		return 0;
+	return binarysearch(nums, 0, numsSize - 1, target);
 }
 
 int main(int argc, char *argv[])
 {
-	int nums[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-	int numsSize = 10;
-	int target = 10;
+	int nums[1] = { 0 };
+	int numsSize = 1;
+	int target = 1;
 	int result = searchInsert(nums, numsSize, target);
 	printf("The result is %d\n", result);
 	return 0;
